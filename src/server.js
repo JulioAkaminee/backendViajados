9e2572c72e5cf5fdea66e2940e7e40e5d46ad890
JulioAkaminee <juliooakamine@gmail.com>
2025-03-24 11:04:54 -0300

    corrigindo server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
 const porta = process.env.PORT || 3000;
-=======
->>>>>>> 5737cbf5c0455eba04a1ad9b42a231c769cfc7b5
 
 const cadastro = require("./routes/cadastro");
 const login = require("./routes/login");
@@ -23,11 +20,7 @@ app.use("/api/cadastro", cadastro);
 app.use("/api/login", login);
 app.use("/api/alterarsenha", alterarSenha);
 
-<<<<<<< HEAD
 // Middleware de autenticação
-=======
-// Middleware de verificação de token
->>>>>>> 5737cbf5c0455eba04a1ad9b42a231c769cfc7b5
 app.use(verificarToken);
 
 // Rotas protegidas (exigem token)
@@ -36,11 +29,6 @@ app.use("/api/voos", voos);
 app.use("/api/favoritos", favoritos);
 app.use("/api/alterardados", alterarDados);
 
-<<<<<<< HEAD
 app.listen(porta, () => {
   console.log(`Servidor rodando na porta ${porta}`);
 });
-=======
-// Exporta o app para a Vercel
-module.exports = app;
->>>>>>> 5737cbf5c0455eba04a1ad9b42a231c769cfc7b5
