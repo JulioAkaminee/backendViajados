2af054676a2f2a1b0732016dca1686b32098f83e
JulioAkaminee <juliooakamine@gmail.com>
2025-03-28 12:41:38 -0300

    alterando tempo db

--- src/db/conn.js
+++ src/db/conn.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 30,
     queueLimit: 0,
     ssl: {
         ca: process.env.DB_SSL_CA
